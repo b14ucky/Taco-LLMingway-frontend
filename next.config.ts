044bd6b8292cb216d8cd726a4805648c41d0c1c8
Dominik <101473446+b14ucky@users.noreply.github.com>
2026-02-25 21:52:13 +0100

    chore(next): enable trailingSlash in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   output: "export",
-  /* config options here */
+  trailingSlash: true,
 };
 
 const withNextIntl = createNextIntlPlugin();
